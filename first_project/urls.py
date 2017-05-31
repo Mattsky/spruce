2aa1ef475c71d05dd189bad51fdc64355e860571
Matt N <mnorth@cryptech.co>
2017-05-31 13:31:52 +0100

    Fixing views

--- first_project/urls.py
+++ first_project/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import include
-#from first_app import views
+from first_app import views
 #from app_two import views
 
 urlpatterns = [
