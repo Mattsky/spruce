57341a0cbbcc0c54cc7afb04d894d4209f11ff32
Matt N <mnorth@cryptech.co>
2017-05-31 14:52:11 +0100

    Updating DB model to avoid filling up - better way to design table / unique keys needs research

--- first_app/models.py
+++ first_app/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Hosts(models.Model):
+	id = models.BigAutoField(primary_key=True)
 	hostname = models.CharField(max_length=50,unique=True)
 
 	def __str__(self):
@@ -18,6 +19,7 @@ class HostInfo(models.Model):
 		return str(self.host_address)
 
 class HeldPackageList(models.Model):
+	id = models.BigAutoField(primary_key=True)
 	host_name = models.ForeignKey(Hosts)
 	package = models.CharField(max_length=40)
 	currentver = models.CharField(max_length=40)
@@ -26,6 +28,7 @@ class HeldPackageList(models.Model):
 		return str(self.package)
 
 class InstalledPackageList(models.Model):
+	id = models.BigAutoField(primary_key=True)
 	host_name = models.ForeignKey(Hosts)
 	package = models.CharField(max_length=60)
 	currentver = models.CharField(max_length=60)
@@ -34,6 +37,7 @@ class InstalledPackageList(models.Model):
 		return str(self.package)
 
 class UpdateablePackageList(models.Model):
+	id = models.BigAutoField(primary_key=True)
 	host_name = models.ForeignKey(Hosts)
 	package = models.CharField(max_length=40)
 	currentver = models.CharField(max_length=60)
