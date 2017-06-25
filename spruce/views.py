69c96a0c501de874ce0609791e8319b492f639ec
Matt North <mnorth@cryptech.co>
2017-06-25 09:56:58 +0100

    Better SSH keyfile state checking, cleaned up some templates

--- spruce/views.py
+++ spruce/views.py
@@ -42,125 +42,134 @@ ssh_keyfile = Path(KEYFILE)
 
 # Create your views here.
 
-@login_required
-def index(request):
+def syscheck():
+
+    if ssh_keyfile.is_file() == False:
+        return('The SSH keyfile doesn\'t exist. Please check it, then refresh this page.')
 
     keyfile_permissions = str(oct(os.stat(KEYFILE)[ST_MODE])[-3:])
     process_owner = os.geteuid()
     keyfile_owner = os.stat(KEYFILE)[ST_UID]
-    
+
     print(keyfile_permissions)
     if keyfile_permissions != "600":
 
         print("Bad permissions")
-        messages.error(request, 'The SSH keyfile permissions are wrong. Please check them then return to the index.')
-        return render(request,'spruce/disabled.html')
+        return('The SSH keyfile permissions are wrong. Please check them then refresh this page.')
 
     if process_owner != keyfile_owner:
 
-        messages.error(request, 'The Django process does not have access to the keyfile. Please run it as the correct user, then return to the index.')
+        return('The Django process does not have access to the keyfile. Please run it as the correct user, then refresh this page.')
+
+@login_required
+def index(request):
+
+    check_result = syscheck(request)
+    if check_result:
+        messages.error(request, check_result)
         return render(request,'spruce/disabled.html')
 
-    if ssh_keyfile.is_file():
+    
 
-        list_of_hosts = Hosts.objects.values_list('hostaddr', flat=True)
+    list_of_hosts = Hosts.objects.values_list('hostaddr', flat=True)
+    
+    new_host_list = []
+    
+    for x in list_of_hosts:
+        host_id = Hosts.objects.only('id').get(hostaddr=x)
+        host_port = Hosts.objects.filter(hostaddr=x).values_list('hostport', flat=True)
         
-        new_host_list = []
+        host_name = HostInfo.objects.filter(host_addr_id=host_id).values_list('host_name', flat=True)
         
-        for x in list_of_hosts:
-            host_id = Hosts.objects.only('id').get(hostaddr=x)
-            host_port = Hosts.objects.filter(hostaddr=x).values_list('hostport', flat=True)
+        os_name = HostInfo.objects.filter(host_addr_id=host_id).values_list('os_name', flat=True)
+        os_version = HostInfo.objects.filter(host_addr_id=host_id).values_list('os_version', flat=True)
+
+        new_host_list.append([x, str(host_name[0]), str(os_name[0]), str(os_version[0]), str(host_port[0])]) 
+
+    
+    host_list = {'hosts':new_host_list} 
+
+    if request.method == "POST":
+        if 'address' in request.POST.keys() and request.POST['address']:
+            target_address = request.POST['address']
+            scan_address = target_address.split(':')[0]
+            scan_port_temp = target_address.split(':')[1]
+            scan_port = scan_port_temp.split('}')[0]
             
-            host_name = HostInfo.objects.filter(host_addr_id=host_id).values_list('host_name', flat=True)
+            rescan(scan_address, scan_port, AUTH_USER, KEYFILE)
+            list_of_hosts = Hosts.objects.values_list('hostaddr', flat=True)
+    
+            new_host_list = []
+
+            for x in list_of_hosts:
+                host_id = Hosts.objects.only('id').get(hostaddr=x)
+                host_port = Hosts.objects.filter(hostaddr=x).values_list('hostport', flat=True)
+                host_name = HostInfo.objects.filter(host_addr_id=host_id).values_list('host_name', flat=True)
+                os_name = HostInfo.objects.filter(host_addr_id=host_id).values_list('os_name', flat=True)
+                os_version = HostInfo.objects.filter(host_addr_id=host_id).values_list('os_version', flat=True)
+
+                new_host_list.append([x, str(host_name[0]), str(os_name[0]), str(os_version[0]), str(host_port[0])]) 
+
+            host_list = {'hosts':new_host_list} 
+            return render(request, 'spruce/index.html', context=host_list)
+
+        if 'delete' in request.POST.keys() and request.POST['delete']:
+            scan_address = request.POST['delete']
+        
+            delete_info(scan_address)
+            list_of_hosts = Hosts.objects.values_list('hostaddr', flat=True)
+    
+            new_host_list = []
             
-            os_name = HostInfo.objects.filter(host_addr_id=host_id).values_list('os_name', flat=True)
-            os_version = HostInfo.objects.filter(host_addr_id=host_id).values_list('os_version', flat=True)
+            for x in list_of_hosts:
+                host_id = Hosts.objects.only('id').get(hostaddr=x)
+                host_port = Hosts.objects.filter(hostaddr=x).values_list('hostport', flat=True)
+                host_name = HostInfo.objects.filter(host_addr_id=host_id).values_list('host_name', flat=True)
+                os_name = HostInfo.objects.filter(host_addr_id=host_id).values_list('os_name', flat=True)
+                os_version = HostInfo.objects.filter(host_addr_id=host_id).values_list('os_version', flat=True)
 
-            new_host_list.append([x, str(host_name[0]), str(os_name[0]), str(os_version[0]), str(host_port[0])]) 
+                new_host_list.append([x, str(host_name[0]), str(os_name[0]), str(os_version[0]), str(host_port[0])]) 
 
-        
-        host_list = {'hosts':new_host_list} 
+            host_list = {'hosts':new_host_list} 
+            return render(request, 'spruce/index.html', context=host_list)
 
-        if request.method == "POST":
-            if 'address' in request.POST.keys() and request.POST['address']:
-                target_address = request.POST['address']
-                scan_address = target_address.split(':')[0]
-                scan_port_temp = target_address.split(':')[1]
-                scan_port = scan_port_temp.split('}')[0]
-                
-                rescan(scan_address, scan_port, AUTH_USER, KEYFILE)
-                list_of_hosts = Hosts.objects.values_list('hostaddr', flat=True)
-        
-                new_host_list = []
-
-                for x in list_of_hosts:
-                    host_id = Hosts.objects.only('id').get(hostaddr=x)
-                    host_port = Hosts.objects.filter(hostaddr=x).values_list('hostport', flat=True)
-                    host_name = HostInfo.objects.filter(host_addr_id=host_id).values_list('host_name', flat=True)
-                    os_name = HostInfo.objects.filter(host_addr_id=host_id).values_list('os_name', flat=True)
-                    os_version = HostInfo.objects.filter(host_addr_id=host_id).values_list('os_version', flat=True)
-
-                    new_host_list.append([x, str(host_name[0]), str(os_name[0]), str(os_version[0]), str(host_port[0])]) 
-
-                host_list = {'hosts':new_host_list} 
-                return render(request, 'spruce/index.html', context=host_list)
-
-            if 'delete' in request.POST.keys() and request.POST['delete']:
-                scan_address = request.POST['delete']
+        if 'scan_all' in request.POST.keys() and request.POST['scan_all']:
             
-                delete_info(scan_address)
-                list_of_hosts = Hosts.objects.values_list('hostaddr', flat=True)
-        
-                new_host_list = []
-                
-                for x in list_of_hosts:
-                    host_id = Hosts.objects.only('id').get(hostaddr=x)
-                    host_port = Hosts.objects.filter(hostaddr=x).values_list('hostport', flat=True)
-                    host_name = HostInfo.objects.filter(host_addr_id=host_id).values_list('host_name', flat=True)
-                    os_name = HostInfo.objects.filter(host_addr_id=host_id).values_list('os_name', flat=True)
-                    os_version = HostInfo.objects.filter(host_addr_id=host_id).values_list('os_version', flat=True)
+            list_of_scan_targets = []
+            list_of_addresses = Hosts.objects.values_list('hostaddr', flat=True)
+            list_of_ports = Hosts.objects.values_list('hostport', flat=True)
+            for x in range(0, len(list_of_addresses)):
+                #list_of_hosts.append([ list_of_addresses[x] , list_of_ports[x] ])
+                list_of_scan_targets.append([list_of_addresses[x], list_of_ports[x]])
 
-                    new_host_list.append([x, str(host_name[0]), str(os_name[0]), str(os_version[0]), str(host_port[0])]) 
+            multi_system_rescan(list_of_scan_targets, AUTH_USER, KEYFILE)
 
-                host_list = {'hosts':new_host_list} 
-                return render(request, 'spruce/index.html', context=host_list)
+            new_host_list = []
+            
+            for x in list_of_hosts:
+                host_id = Hosts.objects.only('id').get(hostaddr=x)
+                host_port = Hosts.objects.filter(hostaddr=x).values_list('hostport', flat=True)
+                host_name = HostInfo.objects.filter(host_addr_id=host_id).values_list('host_name', flat=True)
+                os_name = HostInfo.objects.filter(host_addr_id=host_id).values_list('os_name', flat=True)
+                os_version = HostInfo.objects.filter(host_addr_id=host_id).values_list('os_version', flat=True)
 
-            if 'scan_all' in request.POST.keys() and request.POST['scan_all']:
-                
-                list_of_scan_targets = []
-                list_of_addresses = Hosts.objects.values_list('hostaddr', flat=True)
-                list_of_ports = Hosts.objects.values_list('hostport', flat=True)
-                for x in range(0, len(list_of_addresses)):
-                    #list_of_hosts.append([ list_of_addresses[x] , list_of_ports[x] ])
-                    list_of_scan_targets.append([list_of_addresses[x], list_of_ports[x]])
+                new_host_list.append([x, str(host_name[0]), str(os_name[0]), str(os_version[0]), str(host_port[0])]) 
 
-                multi_system_rescan(list_of_scan_targets, AUTH_USER, KEYFILE)
-
-                new_host_list = []
-                
-                for x in list_of_hosts:
-                    host_id = Hosts.objects.only('id').get(hostaddr=x)
-                    host_port = Hosts.objects.filter(hostaddr=x).values_list('hostport', flat=True)
-                    host_name = HostInfo.objects.filter(host_addr_id=host_id).values_list('host_name', flat=True)
-                    os_name = HostInfo.objects.filter(host_addr_id=host_id).values_list('os_name', flat=True)
-                    os_version = HostInfo.objects.filter(host_addr_id=host_id).values_list('os_version', flat=True)
-
-                    new_host_list.append([x, str(host_name[0]), str(os_name[0]), str(os_version[0]), str(host_port[0])]) 
-
-                host_list = {'hosts':new_host_list} 
-                return render(request, 'spruce/index.html', context=host_list)
+            host_list = {'hosts':new_host_list} 
+            return render(request, 'spruce/index.html', context=host_list)
 
 
-        return render(request,'spruce/index.html',context=host_list)
+    return render(request,'spruce/index.html',context=host_list)
 
-    else:
-
-        messages.error(request, 'The SSH keyfile was not found. Please check it exists then return to the index.')
-        return render(request,'spruce/disabled.html')
 
 @login_required
 def held(request):
     
+    check_result = syscheck(request)
+    if check_result:
+        messages.error(request, check_result)
+        return render(request,'spruce/disabled.html')
+
     try:
 
         syshost = request.GET['hostaddr']
@@ -184,6 +193,11 @@ def held(request):
 
 @login_required
 def updates(request):
+
+    check_result = syscheck(request)
+    if check_result:
+        messages.error(request, check_result)
+        return render(request,'spruce/disabled.html')
 
     try:
 
@@ -209,6 +223,11 @@ def updates(request):
 @login_required
 def installed(request):
 
+    check_result = syscheck(request)
+    if check_result:
+        messages.error(request, check_result)
+        return render(request,'spruce/disabled.html')
+
     try:
 
         syshost = request.GET['hostaddr']
@@ -232,53 +251,44 @@ def installed(request):
 @login_required
 def scan(request):
 
-    keyfile_permissions = str(oct(os.stat(KEYFILE)[ST_MODE])[-3:])
-    process_owner = os.geteuid()
-    keyfile_owner = os.stat(KEYFILE)[ST_UID]
-    
-    print(keyfile_permissions)
-    if keyfile_permissions != "600":
-
-        print("Bad permissions")
-        messages.error(request, 'The SSH keyfile permissions are wrong. Please check them then return to the index.')
+    check_result = syscheck(request)
+    if check_result:
+        messages.error(request, check_result)
         return render(request,'spruce/disabled.html')
 
-    if process_owner != keyfile_owner:
 
-        messages.error(request, 'The Django process does not have access to the keyfile. Please run it as the correct user, then return to the index.')
-        return render(request,'spruce/disabled.html')
+    try:
 
-    if ssh_keyfile.is_file():
+        if request.method == "POST":
+            if 'address' in request.POST.keys() and request.POST['address']:
+                if 'sshport' not in request.POST:
+                    scan_port = '22'
+                else:
+                    scan_port = request.POST['sshport']
+                scan_address = request.POST['address']
+                rescan(scan_address, scan_port, AUTH_USER, KEYFILE)
+                messages.success(request, 'Scan successful - details added.')
+                return render(request, 'spruce/scan.html')
 
-        try:
+            
+        return render(request,'spruce/scan.html')
 
-            if request.method == "POST":
-                if 'address' in request.POST.keys() and request.POST['address']:
-                    if 'sshport' not in request.POST:
-                        scan_port = '22'
-                    else:
-                        scan_port = request.POST['sshport']
-                    scan_address = request.POST['address']
-                    rescan(scan_address, scan_port, AUTH_USER, KEYFILE)
-                    messages.success(request, 'Scan successful - details added.')
-                    return render(request, 'spruce/scan.html')
+    except OSError as e:
+        messages.error(request, 'The remote system could not be contacted.')
+        return render(request,'spruce/scan.html')
+    except NoValidConnectionsError as e:
+        messages.error(request, e)
+        return render(request,'spruce/scan.html')
 
-                
-            return render(request,'spruce/scan.html')
-
-        except OSError as e:
-            messages.error(request, 'The remote system could not be contacted.')
-            return render(request,'spruce/scan.html')
-        except NoValidConnectionsError as e:
-            messages.error(request, e)
-            return render(request,'spruce/scan.html')
-
-    else:
-
-        return render(request,'spruce/disabled.html')
+ 
 
 @login_required
 def update_history(request):
+
+    check_result = syscheck(request)
+    if check_result:
+        messages.error(request, check_result)
+        return render(request,'spruce/disabled.html')
 
     try:
 
@@ -302,56 +312,39 @@ def update_history(request):
 @login_required
 def upload_file(request):
 
-    keyfile_permissions = str(oct(os.stat(KEYFILE)[ST_MODE])[-3:])
-    process_owner = os.geteuid()
-    keyfile_owner = os.stat(KEYFILE)[ST_UID]
-    
-    print(keyfile_permissions)
-    if keyfile_permissions != "600":
-
-        print("Bad permissions")
-        messages.error(request, 'The SSH keyfile permissions are wrong. Please check them then return to the index.')
+    check_result = syscheck(request)
+    if check_result:
+        messages.error(request, check_result)
         return render(request,'spruce/disabled.html')
 
-    if process_owner != keyfile_owner:
+    try:
+        if request.method == "POST":
+            system_list = []
+            f = request.FILES['hostFile'] # Grab uploaded file for parsing
+            for line in f:
+                teststring = line.decode("utf-8")
 
-        messages.error(request, 'The Django process does not have access to the keyfile. Please run it as the correct user, then return to the index.')
-        return render(request,'spruce/disabled.html')
+                # Convert bytes literal to string so we can breathe easier..
+                # Check line isn't a section header
+                if '[' not in teststring:
+                    # Check line isn't totally empty
+                    if teststring !='\r\n':
+                        if "host" in teststring:
+                            ip_address_temp = re.search(r'ansible_host=(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})', teststring)
+                            ip_address = ip_address_temp.group(1)
+                        
+                        if "port" in teststring:
+                            connect_port_temp = re.search(r'ansible_port=(\d+)', teststring)
+                            connect_port = connect_port_temp.group(1)
+                        else:
+                            connect_port=22
+                        
+                        system_list.append([ip_address, str(connect_port)])
 
-    if ssh_keyfile.is_file():
-    
-        try:
-            if request.method == "POST":
-                system_list = []
-                f = request.FILES['hostFile'] # Grab uploaded file for parsing
-                for line in f:
-                    teststring = line.decode("utf-8")
-
-                    # Convert bytes literal to string so we can breathe easier..
-                    # Check line isn't a section header
-                    if '[' not in teststring:
-                        # Check line isn't totally empty
-                        if teststring !='\r\n':
-                            if "host" in teststring:
-                                ip_address_temp = re.search(r'ansible_host=(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})', teststring)
-                                ip_address = ip_address_temp.group(1)
-                            
-                            if "port" in teststring:
-                                connect_port_temp = re.search(r'ansible_port=(\d+)', teststring)
-                                connect_port = connect_port_temp.group(1)
-                            else:
-                                connect_port=22
-                            
-                            system_list.append([ip_address, str(connect_port)])
-
-                
-                multi_system_scan(system_list, AUTH_USER, KEYFILE)
-                messages.success(request, 'All systems were scanned. Please check the index.')
-            return render(request, 'spruce/upload_inventory.html')
-        except:
-            messages.error(request, 'An error occurred. Please try again.')
-            return render(request, 'spruce/upload_inventory.html')
-
-    else:
-
-        return render(request,'spruce/disabled.html')
+            
+            multi_system_scan(system_list, AUTH_USER, KEYFILE)
+            messages.success(request, 'All systems were scanned. Please check the index.')
+        return render(request, 'spruce/upload_inventory.html')
+    except:
+        messages.error(request, 'An error occurred. Please try again.')
+        return render(request, 'spruce/upload_inventory.html')
